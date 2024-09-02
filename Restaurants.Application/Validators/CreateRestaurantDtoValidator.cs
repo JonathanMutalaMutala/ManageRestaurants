07731fdca5a064369063d55fa67a275303ccd6ca
Mutala Mutala <jonamut12.live@gmail.com>
2024-09-01 20:05:46 -0400

    Add fluent validator

---
+++ Restaurants.Application/Validators/CreateRestaurantDtoValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Restaurants.Application.Restaurants.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Validators
+{
+    public class CreateRestaurantDtoValidator : AbstractValidator<CreateRestaurantDto>
+    {
+        private readonly List<string> validCategories = ["Italian", "Mexican", "Japanese", "American", "Indian"]; 
+        public CreateRestaurantDtoValidator()
+        {
+            RuleFor(dto => dto.Name)
+                .Length(3, 100);
+
+            RuleFor(dto => dto.Description)
+                .NotEmpty().WithMessage("Description is required");
+
+            RuleFor(dto => dto.Category)
+                .Must(validCategories.Contains)
+                .WithMessage("Invalid Category");
+               
+
+            RuleFor(dto => dto.ContactEmail)
+                .EmailAddress()
+                .WithMessage("Please provide a valid email address");            
+        }
+    }
+}

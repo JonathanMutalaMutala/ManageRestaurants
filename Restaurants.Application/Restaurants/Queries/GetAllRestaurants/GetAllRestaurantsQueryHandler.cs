98bdcecdd367e524cce072b2b8df06cf6e7bb981
Mutala Mutala <jonamut12.live@gmail.com>
2024-09-01 20:44:15 -0400

    Get All Restaurants cqrs pattern.

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants
+{
+    public class GetAllRestaurantsQueryHandler : IRequestHandler<GetAllRestaurantsQuery, IEnumerable<RestaurantDto>>
+    {
+        private readonly ILogger<GetAllRestaurantsQueryHandler> logger;
+        private IMapper mapper;
+        private IRestaurantsRepository restaurantsRepository; 
+       
+        public GetAllRestaurantsQueryHandler(ILogger<GetAllRestaurantsQueryHandler> logger, IMapper mapper, IRestaurantsRepository restaurantsRepository)
+        {
+            this.logger = logger;
+            this.mapper = mapper;
+            this.restaurantsRepository = restaurantsRepository;
+        }
+
+        public async Task<IEnumerable<RestaurantDto>> Handle(GetAllRestaurantsQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Getting all restaurants");
+            var restaurants = await restaurantsRepository.GetAllAsync();
+
+            var restaurantDtos = mapper.Map<IEnumerable<RestaurantDto>>(restaurants); ;
+
+            return restaurantDtos!;
+        }
+    }
+}

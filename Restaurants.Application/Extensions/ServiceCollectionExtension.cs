797a6428cd6b006f12ef12f18f55eb30484bb3af
Mutala Mutala <jonamut12.live@gmail.com>
2024-09-01 20:52:40 -0400

    Add Cqrs patterns for get single restaurant

--- Restaurants.Application/Extensions/ServiceCollectionExtension.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtension.cs
@@ -10,9 +10,10 @@ namespace Restaurants.Application.Extensions
     {
         public static void AddApplication(this IServiceCollection services)
         {
-            var applicationAssembly = typeof(ServiceCollectionExtension).Assembly; 
+            var applicationAssembly = typeof(ServiceCollectionExtension).Assembly;
 
-            services.AddScoped<IRestaurantsService, RestaurantsService>();
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(applicationAssembly)); 
+
             services.AddAutoMapper(applicationAssembly);
 
             services.AddValidatorsFromAssembly(applicationAssembly)
---
+++ Restaurants.Application/Restaurants/Queries/GetRestaurantById/GetRestaurantByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.Application.Restaurants.Queries.GetRestaurantById
+{
+    public class GetRestaurantByIdQueryHandler : IRequestHandler<GetRestaurantByIdQuery, RestaurantDto?>
+    {
+        private readonly ILogger<GetAllRestaurantsQueryHandler> logger;
+        private IMapper mapper;
+        private IRestaurantsRepository restaurantsRepository;
+
+        public GetRestaurantByIdQueryHandler(ILogger<GetAllRestaurantsQueryHandler> logger, IMapper mapper, IRestaurantsRepository restaurantsRepository)
+        {
+            this.logger = logger;
+            this.mapper = mapper;
+            this.restaurantsRepository = restaurantsRepository;
+        }
+
+        public async Task<RestaurantDto?> Handle(GetRestaurantByIdQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Getting single restaurant");
+            var restaurant = await restaurantsRepository.GetByIdAsync(id);
+
+            var restaurantDto = mapper.Map<RestaurantDto>(restaurant);
+
+            return restaurantDto;
+        }
+    }
+}
